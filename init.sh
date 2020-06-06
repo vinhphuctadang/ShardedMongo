336a0fe4272e71411e5cb4a83ec4a51dea8df5d3
foo <vinhphuctadang@gmail.com>
2020-06-06 22:34:39 +0700

    fixed: sharding first before indexing

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ echo 'Clearing containers'
 docker-compose down --volumes # Remove running containers
 docker-compose up -d # Starts all dockerized mongos, there are 1 config server, 3 replica sets and a router
 echo 'Waiting for containers to complete booting up'
-sleep 10 # waiting for booting up
+sleep 20 # waiting for booting up
 
 # Folowwing are run, assumed that docker containers work fine
 # Firstly, add custom config to config server
