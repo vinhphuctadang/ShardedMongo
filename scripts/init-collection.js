bfd553036df1c0b3f5dcfbaa6e996ac2ae8fd2ee
foo <vinhphuctadang@gmail.com>
2020-06-07 14:01:35 +0700

    updated: init-collections: index first, before sharding

--- scripts/init-collection.js
+++ scripts/init-collection.js
@@ -2,7 +2,7 @@ sh.enableSharding('test')
 use test
 db.cars.drop() // drop collection for creation safety
 db.createCollection('cars')
-
-sh.shardCollection('test.cars', {id: 'hashed'}, false, {numInitialChunks: 2}) // sharding first
 db.cars.createIndex({id:1}, {unique: true})
+sh.shardCollection('test.cars', {id: 'hashed'}, false, {numInitialChunks: 2}) // sharding first
+
 db.cars.getShardDistribution()
